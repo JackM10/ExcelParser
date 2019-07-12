2456994ea37def9267837940ba18f2b231e27409
Jack <jack_10@mail.ru>
2019-07-12 18:47:11 +0300

    Very simple example of using XLSX parser ExcelDataReader

---
+++ ExcelParser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using static System.Console;
+using ExcelDataReader;
+
+namespace _1ListingConsoleApp
+{
+    class Program
+    {
+        private static string _pathToXlsx = @"D:\1.xlsx";
+
+        static void Main(string[] args)
+        {
+            ExcelDataReaderImplementation();
+        }
+
+        private static void ExcelDataReaderImplementation()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = File.Open(_pathToXlsx, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    //do
+                    //{
+                    //    while (reader.Read())
+                    //    {
+                    //        WriteLine(reader.GetString(1));
+                    //    }   
+                    //} while (reader.NextResult());
+
+                    var resaltAsDataSet = reader.AsDataSet();
+                    // to take data - resaltAsDataSet.Tables[30].Columns[1].Table.Rows[2].ItemArray // .Table[i] - tab index, .Columns[i] - column index, .Rows[i] - row index
+                }
+            }
+        }
+    }
+}
